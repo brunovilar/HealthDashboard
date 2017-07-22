8e74fee13598f72a744ee497b942939b820f81cd
Bruno Vilar <bruno@kraho.org>
2017-07-22 13:46:48 -0300

    Include filter functions to interact with dashboard

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ from functions import *
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
-import plotly.graph_objs as go
-
 from datetime import datetime
 import pandas as pd
+from dash.dependencies import Input, Output, State
 
 
 # ====================================
@@ -34,73 +32,87 @@ history[settings.COLUMN_ANNOTATION] = (
 history[settings.COLUMN_ITEM_DESCRIPTION] = create_item_description(history)
 history = history.sort_values(by=settings.COLUMN_DATE)
 
-# ===================
-# Dashboard Creation
-# ===================
+
+exam_item_options = [{'label': row[settings.COLUMN_TITLE],
+                      'value': row[settings.COLUMN_ITEM]}
+                     for i, row in history.iterrows()]
+
+
+covered_years = history["date"].apply(lambda x: x.year)
 
 app = dash.Dash()
 
-layout_base = dict(
-    autosize=True,
-    height=500,
-    font=dict(color='#CCCCCC'),
-    titlefont=dict(color='#CCCCCC', size='14'),
-    margin=dict(
-        l=35,
-        r=35,
-        b=35,
-        t=45
-    ),
-    hovermode="closest",
-    plot_bgcolor="#191A1A",
-    paper_bgcolor="#020202",
-    legend=dict(font=dict(size=10), orientation='h'),
-    mapbox=dict(
-        style="dark",
-        center=dict(
-            lon=-78.05,
-            lat=42.54
-        ),
-        zoom=7,
-    )
-)
 
-items = history[settings.COLUMN_ITEM].unique()  # e.g., ["colesterol_total", "ldl", "hdl"]
-ratio_items = [("triglicerideos", "hdl")]
+app.css.append_css({'external_url': 'stylesheet.css'})
+app.layout = html.Div(id="health_dashboard", children=
+              [
+                  html.H1(children='Health Dashboard')
+              ] + [
+                  html.Div(
+                      [
+                          html.P('Filtrar o período de exames exibido:'),
+                          dcc.RangeSlider(
+                              id='year_slider',
+                              min=min(covered_years),
+                              max=max(covered_years),
+                              value=(min(covered_years), max(covered_years)),
+                              marks={str(year): str(year) for year in covered_years}
+                          ),
+                      ],
+                      style={'margin-top': '20'}
+                )
+              ] + [
+                    html.Div(
+                    [
+                        html.P('Mostrar gráficos por grupo de interesse:'),
+                        dcc.RadioItems(
+                            id='exam_item_type',
+                            options=[
+                                {'label': 'Todos ', 'value': 'all'},
+                                {'label': 'Tireoide', 'value': 'tireoide'},  # noqa: E501
+                                {'label': 'Personalizar ', 'value': 'custom'}
+                            ],
+                            value='all',
+                            labelStyle={'display': 'inline-block'}
+                        ),
+                        dcc.Dropdown(
+                            id='exam_item',
+                            options=exam_item_options,
+                            multi=True,
+                            value=["ldl"]
+                        ),
+                    ],
+                    className='six columns'
+),
 
-app.layout = html.Div(children=
-                      [
-                          html.H1(children='Health Dashboard')
-                      ] +
-                      [
-                          html.Div(children=
-                                   [
-                                       html.H1(children=name)
-                                   ] +
-                                   [
-                                       create_item_history_plot(history, name, life_events, layout_base)
-                                   ]
-                                   )
-                          for name in items
-                      ] +
-                      [
-                          html.Div(children=
-                                   [
-                                       html.H1(children="{}/{}".format(name_a, name_b))
-                                   ] +
-                                   [
-                                       create_items_ratio_plot(history, name_a, name_b,
-                                                               reference_range="[{\"Desejável\": [0, 2]}]",
-                                                               layout_base=layout_base,
-                                                               life_events=life_events,
-                                                               show_table=False,
-                                                               last_exam_date=settings.LAST_EXAM_DATE)
-                                   ]
-                                   )
-                          for name_a, name_b in ratio_items
-                      ]
+              ] + [
+                  html.Div(id='dynamic-content',
+                           children=html.Div(children=create_dashboard(history, life_events)))
+              ])
 
-                      )
+
+# ====================================
+# Events triggered by interactions
+#  ====================================
+@app.callback(Output('exam_item', 'value'),
+              [Input('exam_item_type', 'value')])
+def display_type(selector):
+    if selector == 'all':
+        return history.name.unique()
+    elif selector == 'tireoide':
+        return ['tsh', 't4', 't3']
+    else:
+        return []
+
+
+@app.callback(Output('dynamic-content', 'children'),
+              [Input('exam_item', 'value'),
+               Input('year_slider', 'value')])
+def update_plots(items_to_display, year_range):
+
+    dataset = filter_dataset(history, items_to_display, year_range)
+
+    return create_dashboard(dataset, life_events)
 
 
 if __name__ == '__main__':
